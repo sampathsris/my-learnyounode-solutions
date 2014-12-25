54a174b665e4e44e6999dc441648f6fcda60e2ef
sampathsris <sampathsris@gmail.com>
2014-12-25 20:23:18 +0530

    Adding solution to exercise 04

---
+++ 04-my-first-async-io.js
@@ -0,0 +1,11 @@
+require('fs').readFile(
+    process.argv[2],
+    function (err, data) {
+        if (!err) {
+            console.log(
+                data.toString()
+                .split('\n')
+                .length - 1);
+        }
+    }
+);
